cc4a71a0f15fb65e162ff9fea6d56b99b288c27f
Arti B <artib.9803@gmail.com>
2024-01-22 00:46:07 -0500

    prcp data

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
+import datetime as dt
 
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
@@ -36,7 +37,7 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 @app.route("/")
-def systems():
+def welcome():
         return (
             f"/api/v1.0/precipitation"
             f"/api/v1.0/stations"
@@ -45,10 +46,28 @@ def systems():
             f"/api/v1.0/<start>/<end>"
         )
 
-
 @app.route("/api/v1.0/precipitation")
+def precipitation():
+
+    session = Session(engine)
+
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    results = session.query(measuring_table.date, measuring_table.prcp).filter(measuring_table.date >= prev_year).all()
+
+    session.close()
+
+    output = []
+    for record in results:
+        output.append(record.prcp)
+
+    return jsonify(output)
+
 
 @app.route("/api/v1.0/stations")
+def stations():
+
+@app.route("/api/v1.0/tobs")
 
 @app.route("/api/v1.0/<start>")
 
