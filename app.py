2baa9c3974bc987ef5349ba7cd2933920297da74
Arti B <artib.9803@gmail.com>
2024-01-22 00:09:55 -0500

    push

--- app.py
+++ app.py
@@ -1,30 +1,45 @@
 # Import the dependencies.
-
-
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
 
-
 # reflect an existing database into a new model
 
 # reflect the tables
 
-
 # Save references to each table
 
-
 # Create our session (link) from Python to the DB
-
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def systems():
+        return (
+            f"/api/v1.0/precipitation"
+            f"/api/v1.0/stations"
+            f"/api/v1.0/tobs"
+            f"/api/v1.0/<start>"
+            f"/api/v1.0/<start>/<end>"
+        )
+
+
+@app.route("/api/v1.0/precipitation")
+
+@app.route("/api/v1.0/stations")
+
+@app.route("/api/v1.0/<start>")
+
+@app.route("/api/v1.0/<start>/<end>")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
