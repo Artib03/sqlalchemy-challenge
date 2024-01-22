46f563aeb2871a6cd4a324b83c513394bc9e40d5
Arti B <artib.9803@gmail.com>
2024-01-22 00:15:22 -0500

    push

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 # Import the dependencies.
 from flask import Flask, jsonify
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
 #################################################
 # Database Setup
 #################################################
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
+Base.prepare(autoload_with=engine)
+#Base.classes.keys()
 
 # Save references to each table
+measuring_table = Base.classes.measurement
+station_table = Base.classes.station
 
 # Create our session (link) from Python to the DB
+session = Session(bind=engine)
+session.commit()
 
 #################################################
 # Flask Setup
